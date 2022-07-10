4000c230e5acff491e34cbc7c0fcd0e210aa84fd
Shubham Sharma <58418875+Shubham0997@users.noreply.github.com>
2022-07-11 00:02:21 +0530

    Delete src directory

--- src/com/jobportal/configurations/SecurityWebIntializer.java
+++
@@ -1,9 +0,0 @@
-package com.jobportal.configurations;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-
-//initializes the project as a spring security project
-public class SecurityWebIntializer extends AbstractSecurityWebApplicationInitializer{
-
-}
